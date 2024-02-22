37eb67e4f197baf50ddc40d19fd55972afa5c2c0
Ben A Lane <benalane@gmail.com>
2024-02-21 17:57:00 -0800

    Add CMake boilerplate, glad dependency, and glfw submodule. Main file is just from openGL tutorial.

---
+++ src/main.cpp
@@ -0,0 +1,58 @@
+#include <glad/glad.h>
+// glad.h must be before glfw3.h
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+int main() {
+    glfwInit();
+
+    // Use OpenGL version 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    // Explicitly use core profile, no need for backwards compatability.
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Needed for MacOS
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    // Create a window object. Args: width, height, name, ...
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Male the window the main context on current thread
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLAD before we call any OpenGL functions.
+    // glfwGetProcAddress defines the correct function pointers based on which OS we're compiling for
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Tell OpenGL the size of the rendering window.
+    // lower left corner x/y and width/height
+    glViewport(0, 0, 800, 600);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Render loop.
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Clean GLFW's resources.
+    glfwTerminate();
+
+    return 0;
+}
