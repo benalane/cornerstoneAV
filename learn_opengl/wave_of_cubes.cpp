a18d81d8d35867bc6a834436d63bf9a3f00723fa
Ben Lane <benalane@bens-air.lan>
2024-03-06 10:08:54 -0800

    Wave of cubes

---
+++ learn_opengl/wave_of_cubes.cpp
@@ -0,0 +1,169 @@
+#include <glad/glad.h>
+// glad.h must be before glfw3.h
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+
+#include "ShaderProgram.h"
+#include "cube_model.h"
+
+// settings
+constexpr unsigned int SCR_WIDTH = 800;
+constexpr unsigned int SCR_HEIGHT = 600;
+
+// Callback for when the framebuffer is resized. We set the viewport again.
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+
+void processInput(GLFWwindow* window);
+
+int main() {
+    glfwInit();
+
+    // Use OpenGL version 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    // Explicitly use core profile, no need for backwards compatability.
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Needed for MacOS
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    // Create a window object. Args: width, height, name, ...
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Male the window the main context on current thread
+    glfwMakeContextCurrent(window);
+
+    // Anytime the user reseizes the window, a callback function is called.
+    // We use that to also adjust the viewport.
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+    // Initialize GLAD before we call any OpenGL functions.
+    // glfwGetProcAddress defines the correct function pointers based on which OS we're compiling for
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Tell OpenGL the size of the rendering window.
+    // lower left corner x/y and width/height
+    // int frameBufferWidth, frameBufferHeight;
+    // glfwGetFramebufferSize(window, &frameBufferWidth, &frameBufferHeight);
+    // glViewport(0, 0, frameBufferWidth, frameBufferHeight);
+
+    // Globals
+    glEnable(GL_DEPTH_TEST);
+
+    ShaderProgram projectionShader("shaders/projection.vs", "shaders/passthru.fs");
+
+    // Generate vertex array object
+    unsigned int vao;
+    glGenVertexArrays(1, &vao);
+
+    // Generate vertex buffer object
+    unsigned int vbo;
+    glGenBuffers(1, &vbo);
+
+    unsigned int ebo;
+    glGenBuffers(1, &ebo);
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    // Copy vertices from first triangle into first vbo
+    glBufferData(GL_ARRAY_BUFFER, getCubeVerticesSize(), getCubeVertices(), GL_STATIC_DRAW);
+    // Instruct OpenGL how to interpret vertex data in vbo, and enable
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, getCubeElementIndexesSize(), getCubeElementIndexes(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // Render loop.
+    // glfwWindowShouldClose checks if the window has been instruted to close
+
+    float scale = 0.25;
+    float decay = 0.9;
+    constexpr unsigned int numBars = 100;
+    std::vector<float> heights;
+    heights.resize(numBars);
+
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        // Render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        // glClearColor function is a state-setting function and glClear is a state-using function in that it uses the current state to retrieve the clearing color from.
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);  // All options: GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT and GL_STENCIL_BUFFER_BIT
+
+        projectionShader.use();
+
+        // create transformations
+        glm::mat4 view = glm::mat4(1.0f);  // make sure to initialize matrix to identity matrix first
+        glm::mat4 projection = glm::mat4(1.0f);
+        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        view = glm::rotate(view, 0.5f, glm::vec3(0.25f, 1.0f, 0.0f));
+
+        // pass transformation matrices to the shader
+        projectionShader.setMat4("projection", projection);  // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+        projectionShader.setMat4("view", view);
+
+        float cTime = glfwGetTime();
+        heights[0] = abs(sin(cTime) + sin(cTime*3) + sin(cTime*0.7) + sin(cTime*2.1));
+
+        for (auto heightRItr = heights.rbegin(); heightRItr != heights.rend() - 1; ++heightRItr) {
+            *heightRItr = decay * *(heightRItr + 1);
+        }
+
+        for (int i = 0; i < numBars; ++i) {
+            // calculate the model matrix for each object and pass it to shader before drawing
+            glm::mat4 model = glm::mat4(1.0f);
+            glm::vec3 position = glm::vec3(0.0f, -1.0 + (0.5 * heights[i]), -1 * i * scale);
+            model = glm::translate(model, position);
+            model = glm::scale(model, glm::vec3(scale, heights[i], scale));
+            projectionShader.setMat4("model", model);
+            glBindVertexArray(vao);
+            glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+        }
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    glfwTerminate();
+
+    return 0;
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+    // glfwGetKey will return GLFW_RELEASE if escape is NOT being pressed
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
